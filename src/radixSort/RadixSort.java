201ce2bf2678b60fe4ff1cc065ea974696c4733a
Angi-Adema <dr.adema@gmail.com>
2026-06-10 09:49:15 -0600

    created highestNum() method to find the greatest number in array.

--- src/radixSort/RadixSort.java
+++ src/radixSort/RadixSort.java
@@ -2,4 +2,20 @@ package radixSort;
 
 public class RadixSort {
 
+	// Find the largest value in an array
+	private static int highestNum(int[] array) {
+		
+		// Variable to hold the highest number
+		int number = 0;
+		
+		// Loop through the array to find the highest number
+		for (int num : array) {
+			if (num > number) {
+				number = num;
+			}
+		}
+		
+		// Return the highest number
+		return number;
+	}
 }
